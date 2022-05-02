57eca462748041107b17adc6a3733955c94f3a09
Eugene Im <eugeneim@Eugenes-MacBook-Air.local>
2022-05-02 11:50:44 -0700

    directory layout for mobile, tablet, large, and desktop

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Steps to remake this project
 2. npm install/yarn add
 3. npm install -D tailwindcss/yarn add -D tailwindcss
 4. npm install @tailwindcss/line-clamp/yarn add @tailwindcss/line-clamp
-
+5. npm install react-icons
+6. npm install axios
+7. npm start
--- src/pages/directoryPage.js
+++ src/pages/directoryPage.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import data from "../MOCK_DATA.json";
-import footer from "../components/Footer";
+import { axios } from "axios";
+import { FaHouseUser } from "react-icons/fa";
 /*
 screens: {
       mb: "376px",
@@ -9,10 +10,36 @@ screens: {
       lg: "1025px",
       xl: "1281px",
     }
+
+Scheme that Steve wants:
+- Dark Green Button
+- Gold Letters inside the button
+- Footer is TBD
+- Hero image/Profile background is TBD
 */
 export default function DirectoryPage() {
   const [realtor, setRealtor] = useState({});
   const [searchTerm, setSearchTerm] = useState("");
+
+  const [realtors, setRealtors] = useState([]);
+
+  useEffect(() => {
+    async function getRealtors() {
+      var mydata = JSON.parse(localStorage.getItem("myData"));
+      const result = await axios.get(
+        "https://localhost:44340/api/directory/realtors/",
+        {
+          headers: {
+            Authorization: `Bearer ${mydata.tokenString}`,
+          },
+        }
+      );
+      const resultRealtors = result.data;
+      setRealtors(resultRealtors);
+      console.log(resultRealtors);
+    }
+    getRealtors();
+  }, []);
 
   function displayProfile(realtor) {
     setRealtor(realtor);
@@ -21,8 +48,8 @@ export default function DirectoryPage() {
     <>
       <div
         className="flex 
-        sm:ml-5 h-screen
-        md:ml-5 mt-5 h-screen
+        sm:ml-5 h-3/4
+        md:ml-5 mt-5 h-3/4
         "
       >
         <div
@@ -32,7 +59,7 @@ export default function DirectoryPage() {
         >
           <h2 className="pb-5">Directory</h2>
           <p className="text-sm mb-2">Search through {data.length} realtors</p>
-          <div className="relative text-gray-600 focus-within:text-gray-400">
+          <div className="relative text-amber-400 focus-within:text-gray-400">
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
               <button
                 type="submit"
@@ -69,18 +96,21 @@ export default function DirectoryPage() {
               pl-10 
               focus:outline-none 
               focus:bg-white 
-              focus:text-gray-900"
+              focus:text-amber-400"
               placeholder="Search for a realtor"
               autoComplete="off"
             />
           </div>
           <div
             className="
-          md:h-4/6"
+          md:h-4/6
+          sm:h-4/6"
           >
             <div
               className="
-            md:h-5/6"
+            md:h-100vh
+            sm:h-4/5
+            lg:h-4/5 ml-2"
             >
               <ul
                 className="
@@ -88,6 +118,11 @@ export default function DirectoryPage() {
               mt-6
               overflow-y-scroll
               h-full
+              sm:
+              mt-6
+              overflow-y-scroll
+              h-full
+              lg: h-5/6
               "
               >
                 {data
@@ -113,6 +148,14 @@ export default function DirectoryPage() {
                     </li>
                   ))}
               </ul>
+              <FaHouseUser
+                className="
+              lg: 
+              h-24
+              w-24
+              ml-5
+              mt-10"
+              ></FaHouseUser>
             </div>
           </div>
         </div>
@@ -123,7 +166,7 @@ export default function DirectoryPage() {
         >
           <div
             className="
-          bg-gradient-to-r from-green-400 to-blue-500 h-36 
+          bg-stone-900 h-36 
           sm:max-w-full 
           md: px-5"
           ></div>
@@ -131,7 +174,8 @@ export default function DirectoryPage() {
             className="
           pt-10 mb: px-5 
           sm:grid grid-cols-2 grid-rows-5
-          md: mt-10"
+          md: mt-10
+          lg: h-4/6"
           >
             <h1
               className="
@@ -142,11 +186,12 @@ export default function DirectoryPage() {
             </h1>
             <img
               className="
-            border-4 border-stone-900
+            border-4 border-amber-400
             rounded-full absolute top-32 
             mb: h-14 w-14 top-45 
             sm: h-20 w-20 top-40
-            md: h-24 w-24 top-40"
+            md: h-24 w-24 top-40
+            lg: h-28 w-28 top-40 ml-5"
               src={realtor.photo}
             ></img>
             <p
@@ -238,23 +283,31 @@ export default function DirectoryPage() {
       </div>
       <footer
         className="
-      mb: max-w-7xl
-      bg-blue-500
-      text-center
-      h-20
-      px-5
-      pt-5
-      text-sm
-      text-white
-      md: 
-      position-absolute
-      m-0
-      p-0
-      inset-y-0 bottom-0
+        mb: 
+        position-absolute
+        bottom-0
+        left-0
+        right-0
+        text-center
+        text-white
+        bg-gray-900
+        md:
+        position-absolute
+        inset-y-0 bottom-0
+        text-center
+        text-base
+        text-white
+        py-5
+        px-5
+        lg:
+        h-28
+        pt-10
+        px-10
+        text-lg
+
       "
       >
-        Intelectual Property of: Amainder Gill, Adrian Genosa, Guarav Joshi, and
-        Eugene Im.
+        © 2022 Copyright
       </footer>
     </>
   );
