25d1cd4266b03754eccc5c4731fe8a4e6d1d60de
Gaurav Joshi <88600800+joshigaurav91@users.noreply.github.com>
2022-05-05 15:24:06 -0700

    newlisting

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from '../pages/landingPage';
-import LoginPage from '../pages/loginPage';
-import RegisterPage from '../pages/registerPage';
-import HomePage from '../pages/homePage';
-import DirectoryPage from '../pages/directoryPage';
-import MyListingsPage from '../pages/myListingsPage';
-import ProfilePage from '../pages/profilePage';
-import NewListingPage from '../pages/newListingPage';
-
-
-
-export default function AppRouter() {
-    return (
-        <Routes>
-            <Route path='/' element={<LandingPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<RegisterPage />} />
-            <Route path='/home' element={<HomePage />} />
-            <Route path='/directory' element={<DirectoryPage />} />
-            <Route path='/listings' element={<MyListingsPage />} />
-            <Route path='/profile' element={<ProfilePage />} />  
-            <Route path='/newlisting' element={<NewListingPage />} />
-        </Routes>
-    )
-=======
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/landingPage";
 import LoginPage from "../pages/loginPage";
@@ -36,6 +9,7 @@ import ProfilePage from "../pages/profilePage";
 import Tabs from "../Tabs/Tabs";
 import FindRealtor from "../pages/findRealtorPage";
 import ListingDetails from "../pages/listingDetailsPage";
+import NewListingPage from '../pages/newListingPage';
 
 export default function AppRouter() {
   return (
@@ -50,7 +24,7 @@ export default function AppRouter() {
       <Route path="/listings/:id" element={<ListingDetails />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/tabs" element={<Tabs />} />
+      <Route path='/newlisting' element={<NewListingPage />} />
     </Routes>
   );
->>>>>>> b6f675461c7624c5d84ec2b0fbd7fa858e48cf7e
 }
